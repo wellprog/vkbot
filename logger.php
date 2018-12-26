605ca01def2ed3984e330bbbeaa5ee6cca982d2c
wellprog <velorkhalilov@gmail.com>
2018-12-26 19:26:51 +0300

    Add spaces

--- logger.php
+++ logger.php
@@ -2,6 +2,6 @@
 
 class Logger {
     public function Write($data) {
-        file_put_contents("log.log", json_encode($data), FILE_APPEND);
+        file_put_contents("log.log", json_encode($data) . "\n\n", FILE_APPEND);
     }
 }
